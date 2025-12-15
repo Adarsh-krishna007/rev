0a2544f6c6618b405b609e68252d67988f1728a8
Adarsh-krishna007 <ak641501@gmail.com>
2025-12-15 16:35:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("/*", cors());
 app.use(cookieParser());
 app.use(express.json());
 
