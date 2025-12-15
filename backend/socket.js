a9c8d398c047b48d209240dafc2af2b90d0d7aed
Adarsh-krishna007 <ak641501@gmail.com>
2025-12-15 16:10:53 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -1,37 +1,42 @@
-import http from "http"
-import express from "express"
 import { Server } from "socket.io"
-const app=express()
-const server=http.createServer(app)
 
-const io=new Server(server,{
-    cors:{
-        origin:"https://revf.onrender.com",
-        methods:["GET","POST"]
-    }
-})
+const userSocketMap = {}
 
-const userSocketMap={}
-
-export const getSocketId=(receiverId)=>{
-return userSocketMap[receiverId]
+export const getSocketId = (receiverId) => {
+    return userSocketMap[receiverId]
 }
 
-io.on("connection",(socket)=>{
-   const userId=socket.handshake.query.userId
-   if(userId!=undefined){
-    userSocketMap[userId]=socket.id
-   }
+export const initializeSocket = (server) => {
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    })
 
- io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+    io.on("connection", (socket) => {
+        console.log(`New socket connection: ${socket.id}`)
+        
+        const userId = socket.handshake.query.userId
+        if (userId != "undefined") {
+            userSocketMap[userId] = socket.id
+            console.log(`User ${userId} connected with socket ${socket.id}`)
+        }
 
+        io.emit('getOnlineUsers', Object.keys(userSocketMap))
+        console.log('Online users:', Object.keys(userSocketMap))
 
-socket.on('disconnect',()=>{
-    delete userSocketMap[userId]
-     io.emit('getOnlineUsers',Object.keys(userSocketMap))  
-})
+        socket.on('disconnect', () => {
+            console.log(`Socket disconnected: ${socket.id}`)
+            if (userId != "undefined") {
+                delete userSocketMap[userId]
+                console.log(`User ${userId} disconnected`)
+            }
+            io.emit('getOnlineUsers', Object.keys(userSocketMap))
+        })
+    })
 
-})
+    return io
+}
 
-
-export {app,io, server}
+export { userSocketMap }
